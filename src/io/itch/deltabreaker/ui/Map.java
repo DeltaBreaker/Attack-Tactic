8b67a133296a718aa51558d51ad5e70b1d61278c
willc <willc@TABLET-PJQC3KCJ>
2024-02-18 00:24:54 -0500

    Positioned minimap and fixed the filler tiles in forrests

--- src/io/itch/deltabreaker/ui/Map.java
+++ src/io/itch/deltabreaker/ui/Map.java
@@ -13,46 +13,41 @@ public class Map {
 
 	private ArrayList<MapPixel> pixels = new ArrayList<>();
 	private Tile[][] tiles;
-		
+
 	public Map(Tile[][] tiles) {
 		this.tiles = tiles;
+		Vector4f half = new Vector4f(0.5f, 0.5f, 0.5f, 1);
+		Vector4f full = new Vector4f(1, 1, 1, 1);
 		for (int x = 0; x < tiles.length; x++) {
-			for (int y = 0; y < tiles[0].length; y++) {				
-				pixels.add(new MapPixel(new Vector3f(x, y, -80), tiles[x][y].getAverageColor()));
+			for (int y = 0; y < tiles[0].length; y++) {
+				if (!tiles[x][y].containsTag(Tile.TAG_FILLER)) {
+					pixels.add(new MapPixel(new Vector3f(94 + x - tiles.length, 86 - y * 0.99f - tiles[0].length, -80), Vector4f.mul(tiles[x][y].getAverageColor(), (tiles[x][y].isSolid()) ? half : full)));
+				}
 			}
 		}
 	}
 
 	public void tick() {
-		for (int x = 0; x < tiles.length; x++) {
-			for (int y = 0; y < tiles[0].length; y++) {
-//				Vector3f tilePosition = Vector3f.div(tiles[x][y].getPosition(), 16);
-//				
-//				float sin = AdvMath.sin[(xRotation + 360) % 360];
-//				float cos = AdvMath.sin[(xRotation + 360 + 90) % 360];
-//				
-//				pixels.get(x * tiles.length + y).position.set(x * rootOfTwo - y * rootOfTwo, y * rootOfTwo + x * rootOfTwo, -80);
-//				pixels.get(x * tiles.length + y).rotation.set(0, 0, -45);
-			}
-		}
+		
 	}
-	
+
 	public void render() {
 		ArrayList<Vector3f> positions = new ArrayList<>();
 		ArrayList<Vector3f> rotations = new ArrayList<>();
 		ArrayList<Vector3f> scales = new ArrayList<>();
 		ArrayList<Vector4f> colors = new ArrayList<>();
-		
-		for(MapPixel p  : pixels) {
+
+		for (MapPixel p : pixels) {
 			positions.add(p.position);
 			rotations.add(p.rotation);
 			scales.add(Vector3f.SCALE_HALF.copy());
 			colors.add(p.color);
 		}
-		
-		BatchSorter.addBatch("pixel.dae", "pixel.png", "static_3d", Material.DEFAULT.toString(), positions, rotations, scales, colors, false, true);
+
+		BatchSorter.addBatch("pixel.dae", "pixel.png", "static_3d", Material.MATTE.toString(), positions, rotations,
+				scales, colors, false, true);
 	}
-	
+
 }
 
 class MapPixel {
